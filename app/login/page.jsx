5eb40f30b314aa3a4a33fdbc4eb57287426e2cb2
Himel Khan <himel.khan6@gmail.com>
2025-12-01 17:10:47 +0600

    Database Fixed

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,18 +1,22 @@
-import { login, signup } from "./actions";
+"use client"
+import { login, signup } from "./actions"
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSet,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
-export default function LoginPage() {
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+function LoginForm() {
   return (
-    <div className=" w-screen h-screen flex flex-col justify-center items-center">
+    <div className=" w-screen h-full flex flex-col justify-center items-center">
       <h1 className="font-sans text-2xl mb-10">Login / Signup</h1>
+
       <form className="w-[25%]">
         <FieldSet>
           <FieldGroup>
@@ -47,11 +51,71 @@ export default function LoginPage() {
           <Button formAction={login} type="submit">
             Log in
           </Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+function SignUpForm() {
+  return (
+    <div className=" w-screen h-full flex flex-col justify-center items-center">
+      <h1 className="font-sans text-2xl mb-10">Login / Signup</h1>
+
+      <form className="w-[25%]">
+        <FieldSet>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="email@email.com"
+                required
+              />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <FieldDescription>
+                Must be at least 6 characters long.
+              </FieldDescription>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="••••••••"
+                required
+                className={"font-mono"}
+              />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+
+        <div className="flex gap-4 mt-6">
           <Button formAction={signup} type="submit">
             Sign up
           </Button>
         </div>
       </form>
     </div>
-  );
+  )
+}
+
+export default function LoginPage() {
+  return (
+    <div className=" w-screen h-screen flex flex-col justify-center items-center">
+      <Tabs defaultValue="login" className="items-center">
+        <TabsList>
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">SignUp</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginForm />
+        </TabsContent>
+        <TabsContent value="signup">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 }
